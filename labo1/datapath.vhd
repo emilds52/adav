b0e3bd6e5b0809340c03eb1ef128fba365bf6f71
emilds52 <emilds@live.com>
2024-03-18 03:36:59 +0100

    Multiplier only worked unsigned. Sign extension added for it to also work signed. Very inefficient. Synthesis will hopefully remove the redundant components.

--- labo1/datapath.vhd
+++ labo1/datapath.vhd
@@ -27,6 +27,18 @@ ARCHITECTURE behavior OF datapath IS
     Co : out std_logic
   );
   END COMPONENT;
+  
+  COMPONENT multiplier IS
+  GENERIC (
+    DATA_WIDTH : integer := 24
+  );
+  PORT (
+    A  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+    B  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
+    Q  : out std_logic_vector(2*DATA_WIDTH-1 downto 0)
+  );
+  END COMPONENT;
+
   -- Señales y registros
   signal reg1,      reg2,      reg3,      reg4,      reg5      : std_logic_vector(23 downto 0);
   signal reg1_comb, reg2_comb, reg3_comb, reg4_comb, reg5_comb : std_logic_vector(23 downto 0);
@@ -63,122 +75,131 @@ BEGIN
     Co => open
   );
 
-Proc_seq : PROCESS (reset, clk) BEGIN
-  IF reset='0' THEN
-    sv1  <= (others => '0');
-    sv2  <= (others => '0');
-    sv3  <= (others => '0');
-    sv4  <= (others => '0');
-    reg1 <= (others => '0');
-    reg2 <= (others => '0');
-    reg3 <= (others => '0');
-    reg4 <= (others => '0');
-    reg5 <= (others => '0');
-  ELSIF (clk'event AND clk='1') THEN
-    sv1  <= sv1_comb;
-    sv2  <= sv2_comb;
-    sv3  <= sv3_comb;
-    sv4  <= sv4_comb; 
-    reg1 <= reg1_comb;
-    reg2 <= reg2_comb;
-    reg3 <= reg3_comb;
-    reg4 <= reg4_comb;
-    reg5 <= reg5_comb;
-  END IF; 
-END PROCESS;
+  multiplicador : multiplier
+  GENERIC MAP (
+    DATA_WIDTH => 24
+  )
+  PORT MAP (
+    A  => mulA,
+    B  => mulB,
+    Q  => m_mulQ
+  );
 
--- Multiplexores
-PROCESS(all) IS BEGIN
-  -- Retener valor de registros por defecto:
-  sv1_comb  <= sv1;
-  sv2_comb  <= sv2;
-  sv3_comb  <= sv3;
-  sv4_comb  <= sv4;
-  reg1_comb <= reg1;
-  reg2_comb <= reg2;
-  reg3_comb <= reg3;
-  reg4_comb <= reg4;
-  reg5_comb <= reg5;
-  -- Entrada 0 por defecto
-  mulA <= (others => '0');
-  mulB <= (others => '0');
-  sumA <= (others => '0');
-  sumB <= (others => '0');
-  CASE comandos(3 downto 0) IS
-    WHEN "0000" =>
-      mulA      <= entradas;
-      mulB      <= b1;
-      reg1_comb <= mulQ;
-    WHEN "0001" =>
-      mulA      <= entradas;
-      mulB      <= b2;
-      reg1_comb <= mulQ;
-      sumA      <= reg1;
-      sumB      <= sv1;
-      reg2_comb <= sumQ;
-    WHEN "0011" =>
-      mulA      <= entradas;
-      mulB      <= b3;
-      reg1_comb <= mulQ;
-      sumA      <= reg1;
-      sumB      <= sv2;
-      reg3_comb <= sumQ; 
-    WHEN "0100" =>
-      mulA      <= entradas;
-      mulB      <= b4;
-      reg1_comb <= mulQ;
-      sumA      <= reg1;
-      sumB      <= sv3;
-      reg4_comb <= sumQ;
-    WHEN "0101" =>
-      mulA      <= reg2;
-      mulB      <= inv_a1;
-      reg2_comb <= mulQ;
-      sumA      <= reg1;
-      sumB      <= sv4;
-      reg5_comb <= sumQ;
-    WHEN "0110" =>
-      mulA      <= reg2;
-      mulB      <= neg_a2;
-      reg1_comb <= mulQ;
-    WHEN "0111" =>
-      mulA      <= reg2;
-      mulB      <= neg_a3;
-      reg1_comb <= mulQ;
-      sumA      <= reg3;
-      sumB      <= reg1;
-      sv1_comb  <= sumQ;
-    WHEN "1000" =>
-      mulA      <= reg2;
-      mulB      <= neg_a4;
-      reg1_comb <= mulQ;
-      sumA      <= reg4;
-      sumB      <= reg1;
-      sv2_comb  <= sumQ;
-    WHEN "1001" =>
-      mulA      <= reg2;
-      mulB      <= neg_a5;
-      reg1_comb <= mulQ;
-      sumA      <= reg5;
-      sumB      <= reg1;
-      sv3_comb  <= sumQ;
-    WHEN "1010" =>
-      mulA      <= entradas;
-      mulB      <= b5;
-      reg3_comb <= mulQ;
-    WHEN "1011" =>
-      sumA      <= reg3;
-      sumB      <= reg1;
-      sv4_comb  <= sumQ;
-    WHEN others =>
-  END CASE;
-END PROCESS;
+  mulQ   <= m_mulQ(39 downto 16);
 
-m_mulQ <= mulA * mulB;
-mulQ   <= m_mulQ(39 downto 16);
-
-flags <= (others => '0');
-
-salidas <= reg2; -- A partir del ciclo 5. Registrarlo?
+  Proc_seq : PROCESS (reset, clk) BEGIN
+    IF reset='0' THEN
+      sv1  <= (others => '0');
+      sv2  <= (others => '0');
+      sv3  <= (others => '0');
+      sv4  <= (others => '0');
+      reg1 <= (others => '0');
+      reg2 <= (others => '0');
+      reg3 <= (others => '0');
+      reg4 <= (others => '0');
+      reg5 <= (others => '0');
+    ELSIF (clk'event AND clk='1') THEN
+      sv1  <= sv1_comb;
+      sv2  <= sv2_comb;
+      sv3  <= sv3_comb;
+      sv4  <= sv4_comb; 
+      reg1 <= reg1_comb;
+      reg2 <= reg2_comb;
+      reg3 <= reg3_comb;
+      reg4 <= reg4_comb;
+      reg5 <= reg5_comb;
+    END IF; 
+  END PROCESS;
+  
+  -- Multiplexores
+  PROCESS(all) IS BEGIN
+    -- Retener valor de registros por defecto:
+    sv1_comb  <= sv1;
+    sv2_comb  <= sv2;
+    sv3_comb  <= sv3;
+    sv4_comb  <= sv4;
+    reg1_comb <= reg1;
+    reg2_comb <= reg2;
+    reg3_comb <= reg3;
+    reg4_comb <= reg4;
+    reg5_comb <= reg5;
+    -- Entrada 0 por defecto
+    mulA <= (others => '0');
+    mulB <= (others => '0');
+    sumA <= (others => '0');
+    sumB <= (others => '0');
+    CASE comandos(3 downto 0) IS
+      WHEN "0000" =>
+        mulA      <= entradas;
+        mulB      <= b1;
+        reg1_comb <= mulQ;
+      WHEN "0001" =>
+        mulA      <= entradas;
+        mulB      <= b2;
+        reg1_comb <= mulQ;
+        sumA      <= reg1;
+        sumB      <= sv1;
+        reg2_comb <= sumQ;
+      WHEN "0011" =>
+        mulA      <= entradas;
+        mulB      <= b3;
+        reg1_comb <= mulQ;
+        sumA      <= reg1;
+        sumB      <= sv2;
+        reg3_comb <= sumQ; 
+      WHEN "0100" =>
+        mulA      <= entradas;
+        mulB      <= b4;
+        reg1_comb <= mulQ;
+        sumA      <= reg1;
+        sumB      <= sv3;
+        reg4_comb <= sumQ;
+      WHEN "0101" =>
+        mulA      <= reg2;
+        mulB      <= inv_a1;
+        reg2_comb <= mulQ;
+        sumA      <= reg1;
+        sumB      <= sv4;
+        reg5_comb <= sumQ;
+      WHEN "0110" =>
+        mulA      <= reg2;
+        mulB      <= neg_a2;
+        reg1_comb <= mulQ;
+      WHEN "0111" =>
+        mulA      <= reg2;
+        mulB      <= neg_a3;
+        reg1_comb <= mulQ;
+        sumA      <= reg3;
+        sumB      <= reg1;
+        sv1_comb  <= sumQ;
+      WHEN "1000" =>
+        mulA      <= reg2;
+        mulB      <= neg_a4;
+        reg1_comb <= mulQ;
+        sumA      <= reg4;
+        sumB      <= reg1;
+        sv2_comb  <= sumQ;
+      WHEN "1001" =>
+        mulA      <= reg2;
+        mulB      <= neg_a5;
+        reg1_comb <= mulQ;
+        sumA      <= reg5;
+        sumB      <= reg1;
+        sv3_comb  <= sumQ;
+      WHEN "1010" =>
+        mulA      <= entradas;
+        mulB      <= b5;
+        reg3_comb <= mulQ;
+      WHEN "1011" =>
+        sumA      <= reg3;
+        sumB      <= reg1;
+        sv4_comb  <= sumQ;
+      WHEN others =>
+    END CASE;
+  END PROCESS;
+  
+  flags <= (others => '0');
+  
+  salidas <= reg2; -- A partir del ciclo 5. Registrarlo?
 
 END behavior;
--- labo1/multiplier.vhd
+++ labo1/multiplier.vhd
@@ -1,5 +1,6 @@
 LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.numeric_std.ALL;
 ENTITY multiplier IS
 GENERIC (
   DATA_WIDTH : integer := 24
@@ -33,32 +34,39 @@ ARCHITECTURE structural OF multiplier IS
   END COMPONENT;
   
   TYPE LOGIC_ARRAY_T IS ARRAY (NATURAL RANGE <>, NATURAL RANGE <>) OF STD_LOGIC;
-  SIGNAL pps       : LOGIC_ARRAY_T(DATA_WIDTH-1 downto 0, DATA_WIDTH-1 downto 0);
-  SIGNAL sum_aux   : LOGIC_ARRAY_T(DATA_WIDTH-3 downto 0, DATA_WIDTH-2 downto 0);
-  SIGNAL carry_aux : LOGIC_ARRAY_T(DATA_WIDTH-2 downto 0, DATA_WIDTH-1 downto 0);
+  SIGNAL pps       : LOGIC_ARRAY_T(DATA_WIDTH*2-1 downto 0, DATA_WIDTH*2-1 downto 0);
+  SIGNAL sum_aux   : LOGIC_ARRAY_T(DATA_WIDTH*2-3 downto 0, DATA_WIDTH*2-2 downto 0);
+  SIGNAL carry_aux : LOGIC_ARRAY_T(DATA_WIDTH*2-2 downto 0, DATA_WIDTH*2-1 downto 0);
+  SIGNAL A_ext     : std_logic_vector(DATA_WIDTH*2-1 downto 0);
+  SIGNAL B_ext     : std_logic_vector(DATA_WIDTH*2-1 downto 0);
+  SIGNAL Q_tmp     : std_logic_vector(DATA_WIDTH*4-1 downto 0);
 
 BEGIN
+
+  A_ext <= std_logic_vector(resize(signed(A), A_ext'length));
+  B_ext <= std_logic_vector(resize(signed(B), B_ext'length));
+
   -- Generate partial products: 
   pps_proc : PROCESS(ALL) BEGIN
-    pp_row : FOR i IN 0 TO DATA_WIDTH-1 LOOP 
-      pp_col : FOR j IN 0 TO DATA_WIDTH-1 LOOP
-        pps(i, j) <= A(i) AND B(j);
+    pp_row : FOR i IN 0 TO DATA_WIDTH*2-1 LOOP 
+      pp_col : FOR j IN 0 TO DATA_WIDTH*2-1 LOOP
+        pps(i, j) <= A_ext(i) AND B_ext(j);
       END LOOP;
     END LOOP;
   END PROCESS;
   
   -- Generate Adders
-  row_gen : FOR i IN 0 TO DATA_WIDTH-2 GENERATE
+  row_gen : FOR i IN 0 TO DATA_WIDTH*2-2 GENERATE
     row_gen_if : IF i = 0 GENERATE
       -- First row is special
       HA_i_INST : half_adder
       PORT MAP (
         A    => pps(1,0),
         B    => pps(0,1),
-        Sum  => Q(1),
+        Sum  => Q_tmp(1),
         Cout => carry_aux(0,0)
       );
-      fa_first_gen : FOR j IN 0 TO DATA_WIDTH-3 GENERATE
+      fa_first_gen : FOR j IN 0 TO DATA_WIDTH*2-3 GENERATE
         FA_i_INST : full_adder
         PORT MAP (
           A    => pps(2,j),
@@ -70,36 +78,36 @@ BEGIN
       END GENERATE;
       HA_end_INST : half_adder
       PORT MAP (
-        A    => pps(DATA_WIDTH-2,DATA_WIDTH-2),
-        B    => pps(DATA_WIDTH-1,DATA_WIDTH-3),
-        Sum  => sum_aux(0, DATA_WIDTH-2),
-        Cout => carry_aux(0,DATA_WIDTH-1)
+        A    => pps(DATA_WIDTH*2-2,DATA_WIDTH*2-2),
+        B    => pps(DATA_WIDTH*2-1,DATA_WIDTH*2-3),
+        Sum  => sum_aux(0, DATA_WIDTH*2-2),
+        Cout => carry_aux(0,DATA_WIDTH*2-1)
       );
-    ELSIF (i = DATA_WIDTH-2) GENERATE
+    ELSIF (i = DATA_WIDTH*2-2) GENERATE
       -- Generate final row
       HA_i_INST : half_adder
       PORT MAP (
         A    => carry_aux(i-1,0),
         B    => sum_aux(i-1,0),
-        Sum  => Q(i+1),
+        Sum  => Q_tmp(i+1),
         Cout => carry_aux(i,0)
       );
-      fa_last_gen : FOR j IN 0 TO DATA_WIDTH-3 GENERATE
+      fa_last_gen : FOR j IN 0 TO DATA_WIDTH*2-3 GENERATE
         FA_i_INST : full_adder
         PORT MAP (
           A    => carry_aux(i,j),
           B    => sum_aux(i-1,j),
           Cin  => carry_aux(i-1,j+1),
-          Sum  => Q(i+j+2),
+          Sum  => Q_tmp(i+j+2),
           Cout => carry_aux(i,j+1)
         );
       END GENERATE;
       HA_end_INST : half_adder
       PORT MAP (
-        A    => pps(DATA_WIDTH-1,DATA_WIDTH-1),
-        B    => carry_aux(i,DATA_WIDTH-2),
-        Sum  => Q(2*DATA_WIDTH-2),
-        Cout => Q(2*DATA_WIDTH-1)
+        A    => pps(DATA_WIDTH*2-1,DATA_WIDTH*2-1),
+        B    => carry_aux(i,DATA_WIDTH*2-2),
+        Sum  => Q_tmp(2*DATA_WIDTH*2-2),
+        Cout => Q_tmp(2*DATA_WIDTH*2-1)
       );
     ELSE GENERATE
       -- Generate all other rows
@@ -107,10 +115,10 @@ BEGIN
       PORT MAP (
         A    => carry_aux(i-1,0),
         B    => sum_aux(i-1,0),
-        Sum  => Q(i+1),
+        Sum  => Q_tmp(i+1),
         Cout => carry_aux(i,0)
       );
-      fa_normal_gen : FOR j IN 0 TO DATA_WIDTH-3 GENERATE
+      fa_normal_gen : FOR j IN 0 TO DATA_WIDTH*2-3 GENERATE
         FA_i_INST : full_adder
         PORT MAP (
           A    => pps(i+2,j),
@@ -122,15 +130,16 @@ BEGIN
       END GENERATE;
       FA_end_INST : full_adder
       PORT MAP (
-        A    => pps(i+1,DATA_WIDTH-1),
-        B    => pps(i+2,DATA_WIDTH-2),
-        Cin  => carry_aux(i-1,DATA_WIDTH-2),
-        Sum  => sum_aux(i, DATA_WIDTH-2), 
-        Cout => carry_aux(i, DATA_WIDTH-1)
+        A    => pps(i+1,DATA_WIDTH*2-1),
+        B    => pps(i+2,DATA_WIDTH*2-2),
+        Cin  => carry_aux(i-1,DATA_WIDTH*2-2),
+        Sum  => sum_aux(i, DATA_WIDTH*2-2), 
+        Cout => carry_aux(i, DATA_WIDTH*2-1)
       );
     END GENERATE;
   END GENERATE;
   
-  Q(0) <= pps(0,0);
+  Q_tmp(0) <= pps(0,0);
+  Q <= Q_tmp(DATA_WIDTH*2-1 downto 0);
 
 END ARCHITECTURE;
